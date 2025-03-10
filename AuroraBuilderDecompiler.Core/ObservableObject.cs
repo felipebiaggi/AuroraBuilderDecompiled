e240d937d7bc099b95c17cc9404174421432e69f
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2025-03-10 19:41:23 -0300

    (feat) first commit

---
+++ AuroraBuilderDecompiler.Core/ObservableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public abstract class ObservableObject: INotifyPropertyChanged 
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Equals(storage, value))
+        {
+            return false;
+        }
+        storage = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected void OnPropertyChanged(params string[] propertyNames)
+    {
+        foreach (string propertyName in propertyNames)
+        {
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
