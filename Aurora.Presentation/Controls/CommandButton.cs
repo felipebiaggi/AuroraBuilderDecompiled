5015c4be72863e8fd1f278d939d720120dbaec1b
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2025-03-13 10:32:52 -0300

    Add Aurora.Presentation project with custom controls
    This commit introduces a new project called "Aurora.Presentation" to the solution, targeting .NET Framework 4.7.2. It includes the `CommandButton.cs` file, which defines a custom button control with dependency properties. The `AssemblyInfo.cs` file has been created to provide assembly metadata, and the `packages.config` file has been updated to include dependencies for `DynamicExpresso.Core` and `Microsoft.CSharp`.
    
    Additionally, changes were made to assembly reference cache files, and the `build.force` and `.signature.p7s` files were updated. These enhancements improve the project's presentation layer and ensure compatibility with the specified framework version.

---
+++ Aurora.Presentation/Controls/CommandButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Aurora.Presentation.Controls;
+
+namespace Aurora.Presentation.Controls
+{
+    public class CommandButton : Button
+    {
+        public static readonly DependencyProperty CommandTextProperty;
+
+        public static readonly DependencyProperty CommandTextVisibilityProperty;
+
+        public static readonly DependencyProperty CornerRadiusProperty;
+
+        public string CommandText
+        {
+            get
+            {
+                return (string)GetValue(CommandTextProperty);
+            }
+            set
+            {
+                SetValue(CommandTextProperty, value);
+            }
+        }
+
+        public Visibility CommandTextVisibility
+        {
+            get
+            {
+                return (Visibility)GetValue(CommandTextVisibilityProperty);
+            }
+            set
+            {
+                SetValue(CommandTextVisibilityProperty, value);
+            }
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get
+            {
+                return (CornerRadius)GetValue(CornerRadiusProperty);
+            }
+            set
+            {
+                SetValue(CornerRadiusProperty, value);
+            }
+        }
+
+        static CommandButton()
+        {
+            CommandTextProperty = DependencyProperty.Register("CommandText", typeof(string), typeof(CommandButton), new PropertyMetadata((object)null));
+            CommandTextVisibilityProperty = DependencyProperty.Register("CommandTextVisibility", typeof(Visibility), typeof(CommandButton), new PropertyMetadata(Visibility.Visible));
+            CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(CommandButton), new PropertyMetadata(default(CornerRadius)));
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(CommandButton), new FrameworkPropertyMetadata(typeof(CommandButton)));
+        }
+    }
+}
