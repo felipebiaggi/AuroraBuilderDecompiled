836903e61f7b8f29f2273b4db0629ea1d35eba3b
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2025-03-15 21:59:22 -0300

    Refactor UI styles and introduce new user controls
    This commit removes outdated `Aurora.Presentation` styles from `App.xaml` and introduces new user controls for abilities and character management. It expands UI theme options with new accent styles and enhances styling capabilities through `BuilderStyles.xaml` and other resource dictionaries.
    
    New control templates and styles for various UI components, including status bars and menus, improve the overall user experience. The addition of `CharacterOverviewViewModel` facilitates better data management for character attributes. Overall, these changes reflect a significant enhancement in the application's design and usability.

---
+++ Builder.Presentation/ViewModels/CharacterOverview.cs
@@ -0,0 +1,19 @@
+﻿using Builder.Presentation;
+using Builder.Presentation.Models;
+using Builder.Presentation.Models.Collections;
+using Builder.Presentation.ViewModels.Base;
+
+namespace Builder.Presentation.ViewModels
+{
+    public class CharacterOverviewViewModel : ViewModelBase
+    {
+        public Character Character => CharacterManager.Current.Character;
+
+        public SkillsCollection Skills => CharacterManager.Current.Character.Skills;
+
+        public CharacterOverviewViewModel()
+        {
+            SubscribeWithEventAggregator();
+        }
+    }
+}
